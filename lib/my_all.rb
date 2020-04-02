0d183c9cb23336c2cc6566dc406f1770b7c47280
DAZUN XING <dazun.xing@gmail.com>
2020-04-02 03:44:55 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i=0
+  new=[]
+  while i<collection.length
+    new<<yield(collection[i])
+    i=i+1
+  end
+  
+  if new.include?(false)
+    false
+  else
+    true
+  end
 end
